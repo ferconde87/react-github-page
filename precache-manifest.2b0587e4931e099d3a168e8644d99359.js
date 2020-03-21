395e54140aefb6b40def3c52712b6aadb4cf9ff5
fer <ferconde87@gmail.com>
2020-03-21 15:45:03 +0700

    Updates

---
+++ precache-manifest.2b0587e4931e099d3a168e8644d99359.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05408684bc93d2a1a702103fe6c03ba7",
+    "url": "/react-github-page/index.html"
+  },
+  {
+    "revision": "7f85af61ce7e8b354e3b",
+    "url": "/react-github-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d2798b8ed4b4751016e1",
+    "url": "/react-github-page/static/js/2.f97b9f5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-github-page/static/js/2.f97b9f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f85af61ce7e8b354e3b",
+    "url": "/react-github-page/static/js/main.e5a40936.chunk.js"
+  },
+  {
+    "revision": "4e80f6872f0082420c7d",
+    "url": "/react-github-page/static/js/runtime-main.f7808a2c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-github-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
